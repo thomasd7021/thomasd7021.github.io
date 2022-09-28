cd3a2125298602f64737d8fcbb0661fa192cec91
thomasd7021 <114589769+thomasd7021@users.noreply.github.com>
2022-09-28 09:31:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Thomas D."; // update the part BETWEEN the "quotes"
+let userName = "thomasd7021"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
